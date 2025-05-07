ce0b597312b497d53027e1ea23684c3ad7f90d7d
adityathok <aditya0thok@gmail.com>
2025-05-07 09:14:48 +0700

    feat: rincian transaksi dan simbol USD / Rp

--- pages/bank_transaksi.vue
+++ pages/bank_transaksi.vue
@@ -62,13 +62,13 @@
     </div>
   </div>
 
-  <DataTable :value="data.data" size="small" class="text-sm" sortField="nomor" :sortOrder="-1" paginator :rows="25" :rowsPerPageOptions="[25, 50, 100, 500]" stripedRows scrollable>
-    <Column header="#" headerStyle="width:3rem">
+  <DataTable :value="data.data" size="small" class="text-sm" sortField="nomor" :sortOrder="-1" paginator :rows="25" :rowsPerPageOptions="[25, 50, 100, 500]" selectionMode="multiple" stripedRows scrollable>
+    <Column header="#" headerStyle="width:3rem" class="align-top">
       <template #body="slotProps">
           {{ slotProps.index + 1 }}
       </template>
     </Column>
-    <Column field="nomor" header="Tanggal" :sortable="true" class="whitespace-nowrap">
+    <Column field="nomor" header="Tanggal" :sortable="true" class="whitespace-nowrap !align-top">
       <template #body="slotProps">
         {{ slotProps.data.tgl }}
         <span class="hidden">
@@ -76,10 +76,10 @@
         </span>
       </template>
     </Column>
-    <Column field="jenis" header="Jenis">
+    <Column field="jenis" header="Jenis" class="align-top">
       <template #body="slotProps">
 
-        <ul v-if="slotProps.data.cs_main_project" class="list-disc ps-4">
+        <ul v-if="slotProps.data.cs_main_project ||slotProps.data.transaksi_keluar " class="list-decimal ps-4">
           <li v-for="item in slotProps.data.cs_main_project" class="text-xs">
             <div class="font-bold">
               {{ item.tgl_masuk }}
@@ -96,17 +96,8 @@
             <div v-if="item.deskripsi" class="italic">
               "{{ item.deskripsi }}"
             </div>
-            <div v-if="item.bank" v-for="itembank in item.bank" class="mt-1">
-              <Badge v-if="itembank.jenis_transaksi == 'masuk'">
-                {{ itembank.bank }} masuk {{ formatMoney(itembank.nominal) }}
-              </Badge>
-              <Badge v-if="itembank.jenis_transaksi == 'keluar'" severity="danger">
-                {{ itembank.bank }} keluar {{ formatMoney(itembank.nominal) }}
-              </Badge>
-            </div>
-          </li>          
-        </ul>
-        <ul v-if="slotProps.data.transaksi_keluar" class="list-disc ps-4">
+          </li>  
+
           <li v-for="item in slotProps.data.transaksi_keluar" class="text-xs">
             <div class="font-bold">
               {{ item.tgl }}
@@ -117,47 +108,67 @@
             <div class="font-bold">
               {{ formatMoney(item.jml) }}
             </div>
-            <div v-if="item.bank" v-for="itembank in item.bank" class="mt-1">
-              <Badge v-if="itembank.jenis_transaksi == 'masuk'">
-                {{ itembank.bank }} masuk {{ formatMoney(itembank.nominal) }}
-              </Badge>
-              <Badge v-if="itembank.jenis_transaksi == 'keluar'" severity="danger">
-                {{ itembank.bank }} keluar {{ formatMoney(itembank.nominal) }}
-              </Badge>
-            </div>
-          </li>          
+          </li> 
         </ul>
 
       </template>
     </Column>
-    <Column field="keterangan_bank" header="Keterangan Bank"></Column>
-    <Column field="masuk" header="Masuk" class="text-teal-500 dark:text-teal-600">
+    <Column field="jenis" header="Jenis Transaksi" class="align-top">
+      <template #body="slotProps">
+
+        <ul v-if="slotProps.data.cs_main_project || slotProps.data.transaksi_keluar" class="list-decimal ps-4">
+          <li v-for="item in slotProps.data.cs_main_project" class="text-xs">
+            <div v-if="item.bank" v-for="itembank in item.bank" class="mt-1">
+              <div v-if="itembank.jenis_transaksi == 'masuk'" class="bg-sky-500 text-white rounded p-1">
+                {{ itembank.bank }} <div class="font-bold whitespace-nowrap"> {{ formatMoney(itembank.nominal,itembank.bank) }} </div>
+              </div>
+              <div v-if="itembank.jenis_transaksi == 'keluar'" class="bg-red-500 text-white rounded p-1">
+                {{ itembank.bank }} <div class="font-bold whitespace-nowrap"> {{ formatMoney(itembank.nominal,itembank.bank) }} </div>
+              </div>
+            </div>
+          </li>
+          <li v-for="item in slotProps.data.transaksi_keluar" class="text-xs">
+            <div v-if="item.bank" v-for="itembank in item.bank" class="mt-1">
+              <div v-if="itembank.jenis_transaksi == 'masuk'" class="bg-sky-500 text-white rounded p-1">
+                {{ itembank.bank }} <div class="font-bold whitespace-nowrap"> {{ formatMoney(itembank.nominal,itembank.bank) }} </div>
+              </div>
+              <div v-if="itembank.jenis_transaksi == 'keluar'" class="bg-red-500 text-white rounded p-1">
+                {{ itembank.bank }} <div class="font-bold whitespace-nowrap"> {{ formatMoney(itembank.nominal,itembank.bank) }} </div>
+              </div>
+            </div>
+          </li>
+
+        </ul>
+      </template>
+    </Column>
+    <Column field="keterangan_bank" header="Keterangan Bank" class="align-top"></Column>
+    <Column field="masuk" header="Masuk" class="whitespace-nowrap text-sky-500 dark:text-sky-600">
       <template #body="slotProps">
         <template v-if="slotProps.data.jenis_transaksi == 'masuk'">
-          {{ formatMoney(slotProps.data.nominal) }}
+          {{ formatMoney(slotProps.data.nominal,slotProps.data.bank) }}
         </template>
       </template>
     </Column>
-    <Column field="keluar" header="Keluar" class="text-red-500 dark:text-red-600">
+    <Column field="keluar" header="Keluar" class="whitespace-nowrap text-red-500 dark:text-red-600">
       <template #body="slotProps">
         <template v-if="slotProps.data.jenis_transaksi == 'keluar'">
-          {{ formatMoney(slotProps.data.nominal) }}
+          {{ formatMoney(slotProps.data.nominal,slotProps.data.bank) }}
         </template>
       </template>
     </Column>
-    <Column field="saldo" header="Saldo">
+    <Column field="saldo" header="Saldo" class="whitespace-nowrap">
       <template #body="slotProps">
-        {{ formatMoney(slotProps.data.saldo) }}
+        {{ formatMoney(slotProps.data.saldo,slotProps.data.bank) }}
       </template>
     </Column>
     <Column field="act" header="">
       <template #body="slotProps">
         <div class="flex items-center justify-end gap-1">
-          <Button severity="danger" size="small">
-            <Icon name="lucide:trash"/>
-          </Button>
-          <Button severity="secondary" size="small">
+          <Button severity="secondary" size="small" v-tooltip="'Edit'">
             <Icon name="lucide:pen"/>
+          </Button>
+          <Button severity="danger" size="small" v-tooltip="'Hapus'">
+            <Icon name="lucide:trash"/>
           </Button>
         </div>
       </template>
@@ -186,7 +197,7 @@ const banks = ref([
   {value: 'dbs', label: 'DBS'},
   {value: 'jago', label: 'Jago'},
   {value: 'gopay', label: 'Gopay'},
-  {value:'resselercamp', label: 'RESELLERCAMP'},
+  {value:'resellercamp', label: 'RESELLERCAMP'},
   {value:'srsx', label: 'SRSX'},
   {value: 'jenius', label: 'Jenius'},
 ]);
--- utils/formatMoney.ts
+++ utils/formatMoney.ts
@@ -1,4 +1,18 @@
 // utils/formatMoney.ts
-export function formatMoney(value: number) {
-  return new Intl.NumberFormat().format(value);
+export function formatMoney(value: number, currency: string = 'Rp') {
+  // const v = new Intl.NumberFormat().format(value);
+
+  const formatted = new Intl.NumberFormat('id-ID', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(value)
+
+  //jika currency = reseller maka currency = USD
+  if(currency === 'resellercamp'){
+    currency = 'USD' 
+  } else {
+    currency = 'Rp' 
+  }
+
+  return currency +' '+ formatted;
 }
