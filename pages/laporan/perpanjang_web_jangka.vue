79cd495b9ce20559252ae66da93182a038b4bbed
adityathok <aditya0thok@gmail.com>
2025-07-03 14:23:35 +0700

    notfix: project_profit

---
+++ pages/laporan/perpanjang_web_jangka.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Page: laporan/perpanjang_web_jangka
+  </div>
+</template>
+
+<style scoped></style>
--- pages/laporan/project_profit.vue
+++ pages/laporan/project_profit.vue
@@ -6,8 +6,13 @@
       <DatePicker v-model="filter.month_start" view="month" dateFormat="mm/yy" size="small"/>
     </div>
     <div>
-      <label class="mb-1 block text-xs">sampai bulan :</label>
-      <DatePicker v-model="filter.month_end" view="month" dateFormat="mm/yy" size="small"/>
+      <label class="mb-1 block text-xs">jangka tahun :</label>
+      <Select 
+        v-model="filter.jangka_waktu"
+        :options="[{label:'1 Tahun',value:1},{label:'2 Tahun',value:2},{label:'3 Tahun',value:3}]"
+        optionLabel="label" optionValue="value"
+        size="small"
+      />
     </div>
     <div>
       <Button @click="getData">
@@ -17,14 +22,55 @@
     </div>
   </div>
 
-  <ScrollPanel v-if="data.data" style="width: 100%; height: 70vh">
-      <table class="table-fixed border-collapse text-sm">
-        <tbody>
-          <tr v-for="item in data.data" :key="item.id" class="odd:bg-white even:bg-gray-50 dark:odd:bg-gray-900/50 dark:even:bg-gray-950">
-            <td class="align-top border-b border-gray-100 dark:border-gray-800 p-2">
-              {{ item }}
-            </td>
+  <ScrollPanel v-if="data.data" style="width: 100%; height: 70vh" class="my-5">
+      <table class="text-sm w-full">
+        <thead>
+          <tr class="bg-zinc-200 dark:bg-zinc-800">
+            <th class="align-top py-2 px-3 text-left">No</th>
+            <th class="align-top py-2 px-3 text-left">Website</th>
+            <th class="align-top py-2 px-3 text-left">Jenis</th>
+            <th class="align-top py-2 px-3 text-left">Jumlah</th>
+            <th class="align-top py-2 px-3 text-left text-right">Profit</th>
           </tr>
+        </thead>
+        <tbody>
+          <template v-for="(web,i) in data.data">
+
+            <tr class="bg-emerald-100 dark:bg-emerald-700">
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3">  
+                  {{ i+1 }}                
+                </td>
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3">
+                    {{ web.nama_web??'' }}
+                </td>
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3">                  
+                </td>
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3">                  
+                </td>
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3 text-right">
+                  <strong>
+                    {{ formatMoney(web.profit) }}
+                  </strong>
+                </td>
+              </tr>
+
+            <template v-if="web.results" v-for="(item,jenis) in web.results" :key="item.id">
+              <tr class="odd:bg-white even:bg-gray-50 dark:odd:bg-gray-900/50 dark:even:bg-gray-950">
+                <td colspan="2" class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3">                    
+                </td>
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3">
+                  {{ jenis }}
+                </td>
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3">
+                  {{ item.count }}
+                </td>
+                <td class="align-top border-b border-gray-100 dark:border-gray-800 py-2 px-3 text-right">
+                    {{ formatMoney(item.biaya) }}
+                </td>
+              </tr>
+            </template>
+
+          </template>
         </tbody>
       </table>
   </ScrollPanel>
@@ -44,12 +90,18 @@ const route = useRoute();
 const filter = reactive({
     month_start: route.query.month_start || dayjs().subtract(1, 'month').format(''),
     month_end: route.query.month_end || dayjs().format(''),
+    jangka_waktu: route.query.jangka_waktu || 1,
 } as any);
 
 const loading = ref(false);
 const data = ref([] as any);
 const getData = async () => {
   loading.value = true;
+
+  //ubah bulan ke format YYYY-MM
+  filter.month_start = dayjs(filter.month_start).utc().local().format('YYYY-MM');
+  filter.month_end = dayjs(filter.month_end).utc().local().format('YYYY-MM');
+
   try {
     const response = await client('/api/laporan/project_profit',{
       params: filter,
