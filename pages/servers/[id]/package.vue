23b827463c48b24a02bd5b19367d7ed52e22faa2
adityathok <aditya0thok@gmail.com>
2025-07-19 15:32:50 +0700

    add: sync server

--- pages/servers/[id]/package.vue
+++ pages/servers/[id]/package.vue
@@ -5,37 +5,70 @@
       <h1 class="text-xl">Package : {{ dataServer.name }}</h1>
     </div>
     
-    <div class="text-end mb-5">
+    <div class="flex justify-end gap-1 mb-5">
+      <Button v-if="checkboxItems && checkboxItems.length > 0" @click="syncPackageData()" size="small" severity="warn" :loading="loading" v-tooltip.left="'Sync data masing-masing package'">
+        <Icon name="lucide:globe" :class="{ 'animate-spin': loading }" /> Sync Package
+      </Button>
+      <Select v-model="per_page"
+        :options="[20, 50, 100]"
+        @change="getData()" size="small"
+      />
+      <Button @click="syncData()" size="small" severity="success" :loading="loading" v-tooltip.left="'Sync data package dari server'">
+        <Icon name="lucide:cloud-download"/> Sync
+      </Button>
       <Button @click="getData()" size="small" :loading="loading">
         <Icon name="lucide:refresh-cw" :class="{ 'animate-spin': loading }"/> Reload
       </Button>
     </div>
 
-    <DataTable v-if="data && data.length > 0" :value="data"      
-      paginator :rows="20" :rowsPerPageOptions="[20, 50, 100, 500]"
-      size="small" class="text-sm"
-      stripedRows scrollable
-    >
-      <Column field="no" header="No">
-        <template #body="slotProps">
-          {{ slotProps.index + 1 }}
-        </template>
-      </Column>
-      <Column field="data" header="Package">
-        <template #body="slotProps">
-          {{ slotProps.data }}
-        </template>
-      </Column>
-      <Column field="" header="">
-        <template #body="slotProps">
-          <div class="flex justify-end">
-            <Button @click="openDialog(slotProps.data)" size="small">
-              <Icon name="lucide:globe" size="small"/> Lihat
-            </Button>
-          </div>
-        </template>
-      </Column>
-    </DataTable>
+    <div>
+      <DataTable v-if="data.data && data.data.length > 0" :value="data.data"
+        size="small" class="text-sm"
+        stripedRows scrollable
+        v-model:selection="checkboxItems"
+      >
+        <Column selectionMode="multiple" headerStyle="width: 1rem"></Column>
+        <Column field="no" header="No">
+          <template #body="slotProps">
+            {{ slotProps.index + data.from }}
+          </template>
+        </Column>
+        <Column field="name" header="Package"></Column>
+        <Column field="server.name" header="Server"></Column>
+        <Column field="quota" header="Quota"></Column>
+        <Column field="email_daily_limit" header="Email"></Column>
+        <Column field="inode" header="Inode"></Column>
+        <Column field="bandwidth" header="Bandwidth"></Column>
+        <Column field="" header="">
+          <template #body="slotProps">
+            <div class="flex justify-end">
+              <Button @click="openDialog(slotProps.data)" size="small">
+                <Icon name="lucide:settings-2" size="small"/>
+              </Button>
+            </div>
+          </template>
+        </Column>
+      </DataTable>
+      <div class="flex justify-between items-center text-xs mt-3">
+        <div>
+          {{ data.from }} - {{ data.to }} dari {{ data.total }}
+        </div>
+
+        <Paginator
+            :rows="data.per_page"
+            :totalRecords="data.total"
+            @page="onPaginate"
+            :pt="{
+                root: (event: any) => {
+                    const itemForPage =  data.per_page;
+                    const currentPage =  page - 1;
+                    event.state.d_first = itemForPage * currentPage;
+                },
+            }"
+        >
+        </Paginator>
+      </div>
+    </div>
 
     <div v-if="loading">
       <Skeleton v-for="i in 15" class="h-10 mb-1" />
@@ -61,6 +94,8 @@ const client = useSanctumClient()
 
 const route = useRoute()
 const id = Number(route.params.id) || 0
+const page = ref(route.query.page ? Number(route.query.page) : 1);
+const per_page = ref(route.query.per_page ? Number(route.query.per_page) : 20);
 
 const dataServer = ref({} as any)
 const getServers = (data: any) => {
@@ -75,7 +110,13 @@ const getData = async () => {
   error.value = ''
 
   try {
-    const res = await client('/api/servers_packages/'+id)
+    const res = await client('/api/server_packages',{
+      params: {
+        server_id: id,
+        page: page.value,
+        per_page: per_page.value
+      }
+    })
     loading.value = false
     data.value = res
   } catch (e : any) {
@@ -84,6 +125,11 @@ const getData = async () => {
     error.value = e.data?.message
   }
 }
+const onPaginate = (event: { page: number }) => {
+    page.value = event.page + 1;
+    getData()
+};
+
 onMounted(() => {
   getData()
 })
@@ -93,5 +139,44 @@ const selectedItem = ref({} as any);
 const openDialog = (item: any) => {
   selectedItem.value = item
   visibleDialog.value = true
+}
+
+const syncData = async () => {
+  loading.value = true
+  error.value = ''
+
+  try {
+    const res = await client('/api/servers_sync_packages/'+id)
+    loading.value = false
+    getData()
+  } catch (e : any) {
+    const er = useSanctumError(e)
+    loading.value = false
+    error.value = e.data?.message
+  }
+}
+
+const checkboxItems = ref();
+const syncPackageData = async () => {
+  loading.value = true
+  error.value = ''
+
+  //kumpulkan id dari checkboxItems
+  const ids = checkboxItems.value.map((item: any) => item.id);
+
+  //proses satu-persatu bergantian
+  for (const id of ids) {
+    try {
+      const res = await client('/api/servers_sync_package_detail/'+id)
+      loading.value = false
+      getData()
+    } catch (e : any) {
+      const er = useSanctumError(e)
+      loading.value = false
+      error.value = e.data?.message
+    }
+  }
+
+  loading.value = false
 }
 </script>
