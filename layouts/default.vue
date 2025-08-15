02beccdfcf78abdb498a2a7d6f24109d5df79b31
adityathok <aditya0thok@gmail.com>
2025-08-15 15:15:43 +0700

    edit: container bg and max-width

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,9 +8,9 @@
           <DashSidebar/>
         </div>
       </div>
-      <div class="body-wrapper w-full bg-gray-50 dark:bg-black">
+      <div class="body-wrapper w-full bg-gradient-to-tr from-blue-50 to-teal-50 dark:from-black dark:to-gray-950">
         <DashHeader />
-        <div class="mx-auto px-4 py-4 relative">
+        <div class="md:max-w-[1600px] mx-auto px-4 py-4 relative">
           
             <div v-if="$route.meta.title" class="md:hidden mb-5">
               <h1 class="text-lg md:text-xl font-medium text-zinc-900 dark:text-primary-400 capitalize">
