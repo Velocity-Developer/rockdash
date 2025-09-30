143c50e73152fd6e92e787e3f8ef7f8a522921a9
adityathok <aditya0thok@gmail.com>
2025-09-30 15:01:45 +0700

    refactor(Loader): replace spinner with progress bar for better UX
    The spinner was replaced with an indeterminate progress bar that spans the bottom of the screen for better visibility and a more modern loading indicator. This change improves the user experience by providing a more prominent loading state.

--- components/Dash/Loader.vue
+++ components/Dash/Loader.vue
@@ -1,9 +1,6 @@
 <template>
-  <div v-if="loading" class="fixed bottom-5 end-5 z-[999]">
-    <div class="flex gap-2 items-center shadow-lg text-sm text-primary-600 dark:text-primary-200 bg-primary-50 dark:bg-zinc-800 py-1 px-3 rounded">
-      {{ label }}
-      <Icon name="lucide:loader-2" size="2rem" class="animate-spin" />
-    </div>
+  <div v-if="loading" class="fixed bottom-0 end-0 start-[50%] translate-x-[-50%] z-[999] p-2">
+    <ProgressBar mode="indeterminate" style="height: 12px"></ProgressBar>
   </div>
 </template>
 
