f5dd6c658197ea807dd37b14d1f9d2fe7f5808bf
adityathok <aditya0thok@gmail.com>
2025-10-30 14:21:45 +0700

    feat(customer): tambah page customer

--- pages/customer/index.vue
+++ pages/customer/index.vue
@@ -1,137 +1,130 @@
 <template>
-        
-      <div class="w-full flex flex-col md:flex-row gap-2 md:justify-between items-end text-xs mb-5">
-        <form @submit.prevent="refresh();updateRouteParams()" class="hidden md:flex items-end gap-2">
-          <div>
-            <div class="mb-1 opacity-50">Per Page : </div>            
-            <InputText type="number" v-model="filters.per_page" placeholder="per Page" size="small" class="w-[70px] shadow rounded" />
-          </div>
-          <div>
-            <div class="mb-1 opacity-50">Cari</div>
-            <InputText v-model="filters.q" placeholder="Nama, Email, HP" size="small" class="w-[200px] shadow rounded"/>
-          </div>
-          <div>
-            <Button type="submit" size="small" severity="info" class="shadow-md">
-              Go
-            </Button>
-          </div>
-        </form>
-
-        <div class="flex justify-end items-center gap-2">
-          <Button @click="openDialog('add')" size="small" class="shadow-md">
-            <Icon name="lucide:plus-circle" /> <span class="hidden md:inline-block">Tambah</span>
-          </Button>
-          <Button @click="visibleDrawerFilter = true" size="small" severity="info" class="shadow-md">
-            <Icon name="lucide:filter" /> <span class="hidden md:inline-block">Filter</span>
-            <span
-            class="w-2 h-2 bg-yellow-300 rounded-full inline-block absolute top-0 right-0 m-1"
-            v-if="filters.nama || filters.email || filters.hp || filters.alamat"
-            ></span>
-          </Button>
-        </div>
+  
+  <div class="w-full flex flex-col md:flex-row gap-2 md:justify-between items-end text-xs mb-5">
+    <form @submit.prevent="refresh();updateRouteParams()" class="hidden md:flex items-end gap-2">
+      <div>
+        <div class="mb-1 opacity-50">Per Page : </div>            
+        <InputText type="number" v-model="filters.per_page" placeholder="per Page" size="small" class="w-[70px] shadow rounded" />
       </div>
-
-      <Card>
-        <template #content>
-
-          <Message class="w-[14rem] mb-2 shadow hover:shadow-md" severity="info">
-            <div class="text-xs">
-              Total Customer: <span class="font-bold">{{ data.total }}</span>
-            </div>
-          </Message>
-
-          <DataTable @sort="handleSortTable" :value="data.data" size="small" class="text-xs" v-model:selection="selectedRows" stripedRows scrollHeight="70vh" scrollable>
-            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
-            <Column header="#" headerStyle="width:3rem">
-              <template #body="slotProps">
-                  {{ slotProps.index + data.from }}
-              </template>
-            </Column>
-            <Column field="nama" header="Nama" sortable>
-              <template #body="slotProps">
-                <div class="group relative py-1">
-                  <NuxtLink :to="'/customer/'+slotProps.data.id" class="hover:underline block">
-                    {{ slotProps.data.nama }}
-                  </NuxtLink>
-                  <div class="invisible group-hover:visible absolute bottom-[-1rem] inset-x-0 flex item-center">
-                    <Button @click="openDialog('edit',slotProps.data)" class="!text-xs !px-1 !py-0" variant="text" severity="info" size="small">
-                      <Icon name="lucide:pencil" /> Edit
-                    </Button>
-                    <Button @click="confirmDelete(slotProps.data.id)" class="!text-xs !px-1 !py-0" variant="text" severity="danger" size="small">
-                      <Icon name="lucide:trash-2" /> Hapus
-                    </Button>
-                  </div>
-                </div>
-              </template>
-            </Column>
-            <Column field="email" header="Email">
-              <template #body="slotProps">
-                <div class="whitespace-normal">
-                  {{ split_comma(slotProps.data.email) }}
-                </div>
-              </template>
-            </Column>
-            <Column field="hp" header="No. HP">
-              <template #body="slotProps">
-                <div class="whitespace-normal">
-                  {{ split_comma(slotProps.data.hp) }}
-                </div>
-              </template>
-            </Column>
-            <Column field="alamat" header="Alamat">
-              <template #body="slotProps">
-                <div class="max-w-[200px] whitespace-normal">
-                  {{ slotProps.data.alamat }}
-                </div>
-              </template>
-            </Column>
-            <Column field="created_at" sortable header="Tanggal Dibuat">        
-              <template #body="slotProps">
-                  {{ formatTanggal(slotProps.data.created_at) }}
-              </template>
-            </Column>
-            <Column field="act" header="">
-              <template #body="slotProps">
-                <div class="flex item-center gap-1 justify-end">
-                  <Button @click="openDialog('edit',slotProps.data)" severity="info" size="small">
-                    <Icon name="lucide:pencil" />
-                  </Button>
-                  <Button @click="confirmDelete(slotProps.data.id)" severity="danger" size="small">
-                    <Icon name="lucide:trash-2" />
-                  </Button>
-                </div>
-              </template>
-            </Column>
-          </DataTable>
-
-          <div class="flex justify-between items-center text-xs mt-3">
-            <div>
-              {{ data.from }} - {{ data.to }} dari {{ data.total }}
-            </div>
-
-            <Paginator
-                :rows="data.per_page"
-                :totalRecords="data.total"
-                @page="onPaginate"
-                :pt="{
-                    root: (event: any) => {
-                        const itemForPage =  data.per_page;
-                        const currentPage =  page - 1;
-                        event.state.d_first = itemForPage * currentPage;
-                    },
-                }"
-            >
-            </Paginator>
-          </div>
-        </template>
-      </Card>
-
-      <div class="mt-3">
-        <Button v-if="selectedRowsNama" @click="copyToClipboard()" size="small">
-          <Icon name="lucide:copy" /> Copy Nama Customer
+      <div>
+        <div class="mb-1 opacity-50">Cari</div>
+        <InputText v-model="filters.q" placeholder="Nama, Email, HP" size="small" class="w-[200px] shadow rounded"/>
+      </div>
+      <div>
+        <Button type="submit" size="small" severity="info" class="shadow-md">
+          Go
         </Button>
       </div>
-    
+    </form>
+
+    <div class="flex justify-end items-center gap-2">
+      <Button @click="openDialog('add')" size="small" class="shadow-md">
+        <Icon name="lucide:plus-circle" /> <span class="hidden md:inline-block">Tambah</span>
+      </Button>
+      <Button @click="visibleDrawerFilter = true" size="small" severity="info" class="shadow-md">
+        <Icon name="lucide:filter" /> <span class="hidden md:inline-block">Filter</span>
+        <span
+        class="w-2 h-2 bg-yellow-300 rounded-full inline-block absolute top-0 right-0 m-1"
+        v-if="filters.nama || filters.email || filters.hp || filters.alamat"
+        ></span>
+      </Button>
+    </div>
+  </div>
+
+  <Card>
+    <template #content>
+
+      <Message class="w-[14rem] mb-2 shadow hover:shadow-md" severity="info">
+        <div class="text-xs">
+          Total Customer: <span class="font-bold">{{ data.total }}</span>
+        </div>
+      </Message>
+
+      <DataTable @sort="handleSortTable" :value="data.data" size="small" class="text-xs" v-model:selection="selectedRows" stripedRows scrollHeight="70vh" scrollable>
+        <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+        <Column header="#" headerStyle="width:3rem">
+          <template #body="slotProps">
+              {{ slotProps.index + data.from }}
+          </template>
+        </Column>
+        <Column field="nama" header="Nama" sortable>
+          <template #body="slotProps">
+            <div class="group relative py-1">
+              <NuxtLink :to="'/customer/'+slotProps.data.id" class="hover:underline block">
+                {{ slotProps.data.nama }}
+              </NuxtLink>
+              <div class="invisible group-hover:visible absolute bottom-[-1rem] inset-x-0 flex item-center">
+                <Button @click="openDialog('edit',slotProps.data)" class="!text-xs !px-1 !py-0" variant="text" severity="info" size="small">
+                  <Icon name="lucide:pencil" /> Edit
+                </Button>
+                <Button @click="confirmDelete(slotProps.data.id)" class="!text-xs !px-1 !py-0" variant="text" severity="danger" size="small">
+                  <Icon name="lucide:trash-2" /> Hapus
+                </Button>
+              </div>
+            </div>
+          </template>
+        </Column>
+        <Column field="email" header="Email">
+          <template #body="slotProps">
+            <div class="whitespace-normal">
+              {{ split_comma(slotProps.data.email) }}
+            </div>
+          </template>
+        </Column>
+        <Column field="hp" header="No. HP">
+          <template #body="slotProps">
+            <div class="whitespace-normal">
+              {{ split_comma(slotProps.data.hp) }}
+            </div>
+          </template>
+        </Column>
+        <Column field="alamat" header="Alamat">
+          <template #body="slotProps">
+            <div class="max-w-[200px] whitespace-normal">
+              {{ slotProps.data.alamat }}
+            </div>
+          </template>
+        </Column>
+        <Column field="created_at" sortable header="Tanggal Dibuat">        
+          <template #body="slotProps">
+              {{ formatTanggal(slotProps.data.created_at) }}
+          </template>
+        </Column>
+        <Column field="act" header="">
+          <template #body="slotProps">
+            <div class="flex item-center gap-1 justify-end">
+              <Button @click="openDialog('edit',slotProps.data)" severity="info" size="small">
+                <Icon name="lucide:pencil" />
+              </Button>
+              <Button @click="confirmDelete(slotProps.data.id)" severity="danger" size="small">
+                <Icon name="lucide:trash-2" />
+              </Button>
+            </div>
+          </template>
+        </Column>
+      </DataTable>
+
+      <div class="flex justify-between items-center text-xs mt-3">
+        <div>
+          {{ data.from }} - {{ data.to }} dari {{ data.total }}
+        </div>
+
+        <Paginator
+            :rows="data.per_page"
+            :totalRecords="data.total"
+            @page="onPaginate"
+            :pt="{
+                root: (event: any) => {
+                    const itemForPage =  data.per_page;
+                    const currentPage =  page - 1;
+                    event.state.d_first = itemForPage * currentPage;
+                },
+            }"
+        >
+        </Paginator>
+      </div>
+    </template>
+  </Card>
 
   <Drawer v-model:visible="visibleDrawerFilter" header="Filters" position="right">
     <form @submit.prevent="filterSubmit">
@@ -182,7 +175,6 @@ definePageMeta({
     description: 'Daftar Customer',
 })
 import { useDayjs } from '#dayjs'
-import { formatMoney } from '~/utils/formatMoney'
 import { formatDate } from '~/utils/formatDate'
 const dayjs = useDayjs()
 const route = useRoute();
@@ -345,5 +337,6 @@ function formatTanggal(tanggal: string) {
 function split_comma(text: string) {
   return text?text.split(',').join('\n'):'';
 }
-
 </script>
+
+<style scoped></style>
