6b4e5fdddb13099598ce95f952c15bccb8873744
adityathok <aditya0thok@gmail.com>
2025-11-19 09:23:03 +0700

    buat halaman grafik_transaksi_prive

---
+++ pages/laporan/grafik_transaksi_prive.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Page: laporan/grafik_transaksi_prive
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    title: 'Grafik Transaksi Prive',
+})
+const client = useSanctumClient();
+import { useDayjs } from '#dayjs'
+const dayjs = useDayjs()
+const route = useRoute()
+const router = useRouter()
+</script>
