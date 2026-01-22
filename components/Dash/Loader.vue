697df4ab49002628a8f84f16aeddff8b63c1c18d
adityathok <aditya0thok@gmail.com>
2026-01-22 08:47:57 +0700

    refactor(Loader): replace progress bar with spinner and chip component
    Improve loader visibility by using a spinner with label inside a chip component. Also update default label text to be more concise.

--- components/Dash/Loader.vue
+++ components/Dash/Loader.vue
@@ -1,6 +1,10 @@
 <template>
-  <div v-if="loading" class="fixed bottom-0 end-0 start-[50%] translate-x-[-50%] z-[999] p-2">
-    <ProgressBar mode="indeterminate" style="height: 12px"></ProgressBar>
+  <div v-if="loading" class="fixed bottom-0 end-0 z-[999] p-2 flex items-center gap-1">
+    <Chip :label="label" size="small" class="text-sm shadow">              
+        <ProgressSpinner style="width: 30px; height: 30px" strokeWidth="6" fill="transparent"
+                animationDuration=".5s" :aria-label="label" />
+                <span>{{ label }}</span>
+    </Chip>
   </div>
 </template>
 
@@ -9,5 +13,5 @@
     loading: Boolean,
     label: String,
   })
-  const label = computed(() => props.label || 'Memuat data')
+  const label = computed(() => props.label || 'Memuat...')
 </script>
