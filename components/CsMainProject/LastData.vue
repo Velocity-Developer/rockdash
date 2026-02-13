9277072e91a1c0f2ff7a1b3dd3d9245bcd8cf8c9
adityathok <aditya0thok@gmail.com>
2026-02-13 10:52:41 +0700

    feat(CsMainProject/LastData): add loading state and visual enhancements
    - Add loading skeleton and state to improve user experience during data fetch
    - Introduce visual indicator for new entries using green text and star icon
    - Add 'jenis' column to display additional project information
    - Implement 5-second delay before initial data fetch to simulate loading

--- components/CsMainProject/LastData.vue
+++ components/CsMainProject/LastData.vue
@@ -3,20 +3,37 @@
     <div class="text-sm font-bold mb-4 border-b pb-3">
       Billing project terbaru
     </div>
-    <DataTable :value="data" size="small" class="text-xs">
-      <Column field="webhost.nama_web" header="web" />
+
+    <DataTable v-if="!loading" :value="data" size="small" class="text-xs">
+      <Column field="webhost.nama_web" header="web">
+        <template #body="slotProps">
+          <span :class="isNew(slotProps.data.cs_main_project_info.created_at)?'text-green-600':''">{{ slotProps.data.webhost.nama_web }}</span>
+        </template>
+      </Column>
+      <Column field="jenis" header="jenis" />
       <Column field="cs_main_project_info.created_at" header="Tanggal">
         <template #body="slotProps">
             <template v-if="slotProps.data.cs_main_project_info?.created_at">
               {{ formatDate(slotProps.data.cs_main_project_info.created_at,'YYYY-MM-DD HH:mm:ss') }}
 
               <!-- Jika created_at kurang dari 1 jam, maka tampilkan "baru" -->
-              <Badge v-if="isNew(slotProps.data.cs_main_project_info.created_at)" size="small" severity="success">*</Badge>
+              <span v-if="isNew(slotProps.data.cs_main_project_info.created_at)" class="text-green-600 ml-1">
+                <Icon name="lucide:star" />
+              </span>
               
             </template>
         </template>
       </Column>
     </DataTable>
+
+    <div v-if="loading">
+      <div v-for="i in 5" class="flex gap-2 justify-between mt-1">
+        <Skeleton height="2rem"/>
+        <Skeleton height="2rem"/>
+        <Skeleton height="2rem"/>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -24,17 +41,25 @@
 const client = useSanctumClient();
 
 const data = ref([] as any)
+const loading = ref(false)
 const getData = async () => {
+  loading.value = true
   try {
     const res = await client('api/cs_main_project_lastdata');
     data.value = res;
   } catch (error) {
     console.log(error);
+  } finally {
+    loading.value = false
   }
 }
 
 onMounted(() => {
-  getData();
+  loading.value = true
+  //tunggu 5 detik
+  setTimeout(() => {
+    getData()
+  }, 5000)
 })
 
 // Expose the getData method so it can be called from parent component
