813a1c80e1e5b8f1035432c7d96d8c6e9b4a21fa
adityathok <aditya0thok@gmail.com>
2026-03-10 13:23:53 +0700

    feat(WhmcsUserPreview): add webhost details link and data to domain view
    Add a link to the associated webhost from the domain list and display its details (paket, email, hp, wa). Also adjust text size for consistency and remove unused toast import.

--- components/WhmcsUserPreview.vue
+++ components/WhmcsUserPreview.vue
@@ -6,9 +6,9 @@
 
   <div class="space-y-4">
 
-      <div class="p-4 rounded border border-grey-400 dark:border-grey-600">
+      <div class="p-4 rounded border border-grey-400 dark:border-grey-600 text-sm">
         <div class="mb-3 text-emerald-600">
-          <Icon name="lucide:circle-user"/> <span class="ml-1 font-bold text-lg">User</span>
+          <Icon name="lucide:circle-user"/> <span class="ml-1 font-bold text-md">User</span>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-1">
           <div> <span class="font-bold">Name</span> : {{ dataWhmcsUser?.firstname }} {{ dataWhmcsUser?.lastname }} </div> 
@@ -16,25 +16,40 @@
         </div>
       </div>
 
-      <div v-if="dataWhmcsUser.domains && dataWhmcsUser.domains.length > 0" class="p-4 rounded border border-grey-400 dark:border-grey-600">
+      <div v-if="dataWhmcsUser.domains && dataWhmcsUser.domains.length > 0" class="p-4 rounded border border-grey-400 dark:border-grey-600 text-sm">
         <div class="mb-3 text-blue-600">
-          <Icon name="lucide:globe"/> <span class="ml-1 font-bold text-lg">Domains</span>
+          <Icon name="lucide:globe"/> <span class="ml-1 font-bold text-md">Domains</span>
         </div>
        
-        <div v-for="item in dataWhmcsUser.domains" :key="item.id" class="grid grid-cols-1 md:grid-cols-2 gap-1">
-          <div> <span class="font-bold">Domain</span> : {{ item.domain }} </div> 
-          <div> <span class="font-bold">Status</span> : {{ item.status }} </div> 
-          <div> <span class="font-bold">Expiry Date</span> : {{ item.expirydate }} </div> 
-          <div> <span class="font-bold">Registration Date</span> : {{ item.registrationdate }} </div> 
-          <div> <span class="font-bold">Type</span> : {{ item.type }} </div> 
-          <div> <span class="font-bold">Registrar</span> : {{ item.registrar }} </div> 
+        <div v-for="item in dataWhmcsUser.domains" :key="item.id">
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-1">
+            <div> <span class="font-bold">Domain</span> : 
+              {{ item.domain }}
+              <NuxtLink v-if="item.webhost" :to="`/webhost/${item.webhost.id_webhost}`" target="_blank" class="hover:underline text-blue-500">
+                <Icon name="lucide:external-link" />
+              </NuxtLink>
+            </div> 
+            <div> <span class="font-bold">Status</span> : {{ item.status }} </div> 
+            <div> <span class="font-bold">Expiry Date</span> : {{ item.expirydate }} </div> 
+            <div> <span class="font-bold">Registration Date</span> : {{ item.registrationdate }} </div> 
+            <div> <span class="font-bold">Type</span> : {{ item.type }} </div> 
+            <div> <span class="font-bold">Registrar</span> : {{ item.registrar }} </div> 
+          </div>
+          <div v-if="item.webhost" class="border-t py-2 mt-3">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-1">
+              <div> <span class="font-bold">Paket</span> : {{ item.webhost?.paket?.paket }} </div> 
+              <div> <span class="font-bold">Email</span> : {{ item.webhost?.email }} </div> 
+              <div> <span class="font-bold">Hp</span> : {{ item.webhost?.hp }} </div> 
+              <div> <span class="font-bold">Wa</span> : {{ item.webhost?.wa }} </div> 
+            </div>
+          </div>
         </div>
 
       </div>
 
-      <div v-if="dataWhmcsUser.hostings && dataWhmcsUser.hostings.length > 0" class="p-4 rounded border border-grey-400 dark:border-grey-600">
+      <div v-if="dataWhmcsUser.hostings && dataWhmcsUser.hostings.length > 0" class="p-4 rounded border border-grey-400 dark:border-grey-600 text-sm">
         <div class="mb-3 text-amber-600">
-          <Icon name="lucide:server"/> <span class="ml-1 font-bold text-lg">Hostings</span>
+          <Icon name="lucide:server"/> <span class="ml-1 font-bold text-md">Hostings</span>
         </div>
        
         <div v-for="item in dataWhmcsUser.hostings" :key="item.id" class="grid grid-cols-1 md:grid-cols-2 gap-1">
@@ -53,7 +68,6 @@
 
 
 <script setup lang="ts">
-const toast = useToast()
 const client = useSanctumClient();
 const props = defineProps({
   id: {
@@ -70,7 +84,7 @@ const { data: dataWhmcsUser, status: statusWhmcsUser, refresh: refreshWhmcsUser}
     'whmcs_user_preview_-'+props.id,
     () => client('/api/whmcs-user/'+props.id,{
       params: {
-        with: 'domains,hostings'
+        with: 'domains,hostings,domains.webhost,domains.webhost.paket'
       }
     })
 ) as any
