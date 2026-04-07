9f6620986f90e46456b226c42d18f413887a9141
adityathok <aditya0thok@gmail.com>
2026-04-07 09:28:50 +0700

    feat(whmcs-domain): penanganan rekomendassi webhost

--- components/WhmcsDomainWebhostSync.vue
+++ components/WhmcsDomainWebhostSync.vue
@@ -5,15 +5,19 @@
         <ProgressSpinner />
     </div>
 
-    <div v-if="dataSearch && dataSearch.auto_assigned" class="card flex justify-center items-center gap-5 mb-5">
-        <Icon name="lucide:check-check" size="3rem" class="text-teal-500"/> <span>Domain berhasil sync dengan webhost</span>
-    </div>
+    <Message v-if="dataSearch && dataSearch.auto_assigned" class="mt-1 mb-5">
+        <div class="flex justify-center items-center gap-5">
+          <Icon name="lucide:check-check" size="2rem" class="text-teal-500"/> <span>Domain berhasil sync dengan webhost otomatis</span>
+        </div>
+    </Message>
 
-    <div v-if="dataSearch && dataSearch.whmcs_domain && !dataSearch.auto_assigned" class="card flex justify-center items-center gap-5 mb-5">
-        <Icon name="lucide:x" size="3rem" class="text-red-500"/> <span>Webhost tidak ditemukan untuk domain ini</span>
-    </div>
+    <Message v-if="dataSearch && dataSearch.whmcs_domain && !dataSearch.auto_assigned" severity="warn" class="mt-1 mb-5">
+        <div class="flex justify-center items-center gap-5">
+          <Icon name="lucide:x" size="2rem" class="text-red-500"/> <span>Webhost tidak ditemukan otomatis untuk domain ini</span>
+        </div>
+    </Message>
 
-    <div v-if="dataSearch && dataSearch.data && dataSearch.data.length > 0" class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+    <div v-if="dataSearch && dataSearch.data && dataSearch.data.length > 0 && dataSearch.auto_assigned" class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
       <div v-for="item,i in labelWebhost" :key="i" class="space-y-1">
         <div>{{ item.label }}</div>
         <div class="bg-gray-100 dark:bg-slate-800 px-3 py-2 rounded">
@@ -24,8 +28,31 @@
 
     <div 
       v-if="dataSearch && dataSearch.whmcs_domain && !dataSearch.auto_assigned && dataSearch.data"
-      class="border border-teal-200 dark:border-teal-700 p-4 rounded mt-10"
+      class="border border-teal-200 dark:border-teal-700 p-4 rounded mt-10 shadow"
     >
+    <div class="mb-8">      
+      <div class="font-bold text-teal-900 dark:text-teal-100 mb-3">
+        Data WHMCS domain
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3">
+        <div>
+          <div class="text-sm">Domain :</div>
+          {{ dataSearch.whmcs_domain.domain || '-' }}
+        </div>
+        <div>
+          <div class="text-sm">Email :</div>
+          {{ dataSearch.whmcs_domain.user_email || '-' }}
+        </div>
+        <div>
+          <div class="text-sm">Tanggal registrasi :</div>
+          {{ dataSearch.whmcs_domain.registrationdate || '-' }}
+        </div>
+        <div>
+          <div class="text-sm">Tanggal expired :</div>
+          {{ dataSearch.whmcs_domain.expirydate || '-' }}
+        </div>
+      </div>
+    </div>
     <div class="font-bold text-teal-900 dark:text-teal-100 mb-3">
       Rekomendasi webhost yang ditemukan:
     </div>
@@ -65,6 +92,9 @@
               </Button>
             </template>
           </Column>
+          <template #empty>
+            <div class="flex items-center justify-center gap-5 py-6">Tidak ada rekomendasi webhost yang ditemukan !</div>
+          </template>
       </DataTable>
     </div>
 
